5855251bcb62c56071c14d42a50d8006b1917984
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2020-11-06 18:20:21 +0700

    Test

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ package main
 import ("fmt"
 )
 
+func Hello() string {
+  return "Hello, World"
+}
+
 func main() {
-  fmt.Println("Hello World")
+  fmt.Println(Hello())
 }
---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "testing"
+)
+
+func TestHello(t *testing.T) {
+  got := Hello()
+  want := 1
+
+  if got != want {
+    t.Errorf("got %q want %q", got, want)
+  }
+}
